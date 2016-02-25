7cbbabbd35e0ec52a7a8afd7b88391cba9fc2068
Jeremiah Richter <jeremiahrichter@gmail.com>
2016-02-24 19:15:02 -0500

    Add links for Objective 3 to add new Restaurants to the database

--- vagrant/catalog/webserver.py
+++ vagrant/catalog/webserver.py
@@ -22,88 +22,68 @@ class WebServerHandler(BaseHTTPRequestHandler):
         page_str = self.render_str(template, **kwargs)
         self.wfile.write(page_str)
 
-    def get_hello(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
+    def get_form(self):
+        form = cgi.FieldStorage(
+            fp=self.rfile,
+            headers=self.headers,
+            environ={'REQUEST_METHOD': 'POST',
+                     'CONTENT_TYPE': self.headers['Content-Type'],
+                     })
+        return form
+
+    def send_301_html(self):
+        self.send_response(301)
         self.end_headers()
 
-        output = ''
-        output += '<html><body>Hello!'
-        output += '<form method="POST" enctype="mutipart/form-data" action="/hello"' \
-                  '><h2>What would you like me to say?</h2><input name="message" type="text"' \
-                  '><input type="submit" value="Submit"></form>'
-        output += '</body></html>'
-        self.wfile.write(output)
-        print(output)
-
-    def get_hola(self):
+    def get_css(self):
+        f = open(curdir + sep + self.path)
         self.send_response(200)
-        self.send_header('Content-type', 'text/html')
+        self.send_header('Content-type', 'text/css')
         self.end_headers()
-
-        output = ''
-        output += '<html><body>&#161Hola!' \
-                  '<a href="hello"> Back to Hello </a>'
-        output += '<form method="POST" enctype="mutipart/form-data" action="/hello"' \
-                  '><h2>What would you like me to say?</h2><input name="message" type="text"' \
-                  '><input type="submit" value="Submit"></form>'
-        output += '</body></html>'
-        self.wfile.write(output)
-        print(output)
-
-    def post_hello(self):
-        try:
-            self.send_response(301)
-            self.end_headers()
-            form = cgi.FieldStorage(
-                fp=self.rfile,
-                headers=self.headers,
-                environ={'REQUEST_METHOD': 'POST',
-                         'CONTENT_TYPE': self.headers['Content-Type'],
-                         })
-            print(form)
-            message_content = form.getvalue('message')
-
-            output = ''
-            output += '<html><body>'
-            output += '<h1>{}</h1>'.format(message_content)
-            output += '<form method="POST" action="/hello"' \
-                      '><h2>What would you like me to say?</h2><input name="message" type="text"' \
-                      '><input type="submit" value="Submit"></form>'
-            output += '</body></html>'
-            self.wfile.write(output)
-            print(output)
-        except Exception as e:
-            print('An error occurred: {}'.format(e))
+        self.wfile.write(f.read())
 
     def get_restaurants(self):
+        self.send_200_html()
+        session = DBSession()
+        restaurants = session.query(Restaurant).order_by(Restaurant.id.desc()).all()
+        self.render_page('restaurants.html', restaurants=restaurants)
+
+    def get_restaurants_new(self):
+        self.send_200_html()
+        self.render_page('new_restaurant.html')
+
+    def send_200_html(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        session = DBSession()
-        restaurants = session.query(Restaurant).all()
-        self.render_page('restaurants.html', restaurants=restaurants)
+
+    def post_restaurants_new(self):
+        form = self.get_form()
+        new_name = form.getvalue('name')
+        if new_name:
+            session = DBSession()
+            r = Restaurant(name=new_name)
+            session.add(r)
+            session.commit()
+        self.send_response(301)
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Location', '/restaurants')
+        self.end_headers()
 
     def do_GET(self):
         try:
-            if self.path.endswith('/hello'):
-                self.get_hello()
-            elif self.path.endswith('/hola'):
-                self.get_hola()
-            elif self.path.endswith('/restaurants'):
+            if self.path.endswith('/restaurants'):
                 self.get_restaurants()
+            elif self.path.endswith('/restaurants/new'):
+                self.get_restaurants_new()
             elif self.path.endswith('.css'):
-                f = open(curdir + sep + self.path)
-                self.send_response(200)
-                self.send_header('Content-type', 'text/css')
-                self.end_headers()
-                self.wfile.write(f.read())
+                self.get_css()
         except IOError:
             self.send_error(404, 'File not found: {}'.format(self.path))
 
     def do_POST(self):
-        if self.path.endswith('/hello') or self.path.endswith('/hola'):
-            self.post_hello()
+        if self.path.endswith('restaurants/new'):
+            self.post_restaurants_new()
 
 
 def main():
