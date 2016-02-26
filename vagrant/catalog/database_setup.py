d4c3ec1246aa3a675f786e9f1e017b6f9948be18
Jeremiah Richter <jeremiahrichter@gmail.com>
2016-02-26 17:33:24 -0500

    Add many-to-one relationship from MenuItem to Restaurant

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, ForeignKey, Integer, String, Float
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
-from sqlalchemy import create_engine
 
 Base = declarative_base()
 
@@ -9,39 +9,40 @@ Base = declarative_base()
 class Restaurant(Base):
     __tablename__ = 'restaurant'
     name = Column(
-            String(80),
-            nullable=False
+        String(80),
+        nullable=False
     )
     id = Column(
-            Integer,
-            primary_key=True
+        Integer,
+        primary_key=True
     )
+    menu_items = relationship('MenuItem', back_populates='restaurant')
 
 
 class MenuItem(Base):
     __tablename__ = 'menu_item'
     name = Column(
-            String(80),
-            nullable=False
+        String(80),
+        nullable=False
     )
     id = Column(
-            Integer,
-            primary_key=True
+        Integer,
+        primary_key=True
     )
     course = Column(
-            String(250)
+        String(250)
     )
     description = Column(
-            String(250)
+        String(250)
     )
     price = Column(
         Float
     )
     restaurant_id = Column(
-            Integer,
-            ForeignKey('restaurant.id')
+        Integer,
+        ForeignKey('restaurant.id')
     )
-    restaurant = relationship(Restaurant)
+    restaurant = relationship('Restaurant', back_populates='menu_items')
 
 
 engine = create_engine('sqlite:///restaurantmenu.db')
