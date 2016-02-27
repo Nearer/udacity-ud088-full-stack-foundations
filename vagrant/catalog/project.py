2f3132bd14a7e2d2ecb10775cbb176a64886329c
Jeremiah Richter <jeremiahrichter@gmail.com>
2016-02-27 09:48:38 -0500

    Use url_for for urls in templates

--- vagrant/catalog/project.py
+++ vagrant/catalog/project.py
@@ -1,51 +1,65 @@
 import flask
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
-from database_setup import MenuItem, DBSession, Restaurant
+from database_setup import DBSession, Restaurant
 
 session = DBSession()
 
 app = Flask(__name__)
 
 
-@app.route('/restaurants/<int:restaurant_id>/new')
-def new_menu_item(restaurant_id):
+@app.route('/restaurants/<int:r_id>/new')
+def new_menu_item(r_id):
     return 'Create a new menu item here'
 
 
-@app.route('/restaurants/<int:restaurant_id>/<int:item_id>/edit')
-def edit_menu_item(restaurant_id, item_id):
+@app.route('/restaurants/<int:r_id>/<int:item_id>/edit')
+def edit_menu_item(r_id, item_id):
     return 'Edit a menu item here'
 
 
-@app.route('/restaurants/<int:restaurant_id>/<int:item_id>/delete')
-def delete_menu_item(restaurant_id, item_id):
+@app.route('/restaurants/<int:r_id>/<int:item_id>/delete')
+def delete_menu_item(r_id, item_id):
     return 'Delete a menu item here'
 
 
-@app.route('/restaurants/<int:restaurant_id>/')
-def restaurant_menu(restaurant_id):
-    r = session.query(Restaurant).get(restaurant_id)
+@app.route('/restaurants/new')
+def new_restaurant():
+    return render_template('new_restaurant.html')
+
+
+@app.route('/restaurants/<int:r_id>/edit')
+def edit_restaurant(r_id):
+    r = session.query(Restaurant).get(r_id)
+    if r:
+        return render_template('edit_restaurant.html', r=r)
+    else:
+        flask.abort(404)
+
+
+@app.route('/restaurants/<int:r_id>/delete')
+def delete_restaurant(r_id):
+    r = session.query(Restaurant).get(r_id)
+    if r:
+        return render_template('delete_restaurant.html', r=r)
+    else:
+        flask.abort(404)
+
+
+@app.route('/restaurants/<int:r_id>/')
+def restaurant_menu(r_id):
+    r = session.query(Restaurant).get(r_id)
     if r:
         return render_template('menu.html', r=r)
     else:
         flask.abort(404)
 
 
+@app.route('/')
 @app.route('/restaurants/')
 def restaurants():
     restaurants = session.query(Restaurant).all()
     return render_template('restaurants.html', restaurants=restaurants)
-
-
-@app.route('/')
-def main_menu():
-    string = ''
-    for m in session.query(MenuItem).all():
-        string += m.name + '<br>'
-        string += '${}'.format(m.price) + '<br>'
-        string += m.description + '<br><br>'
-    return string
 
 
 if __name__ == '__main__':
